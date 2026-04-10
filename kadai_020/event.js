00ded3de1c9937da5ccfd29c7974247a1a890124
lumber-3150 <xig3150@gmail.com>
2026-04-11 00:04:00 +0900

    再提出２

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -1,6 +1,6 @@
 const clickBtn = document.getElementById('btn');
+const h2 = document.getElementById('text');
 
 clickBtn.addEventListener('click', () =>{
-  const h2 = document.getElementById('text');
   h2.innerHTML ="ボタンをクリックしました";
 })
